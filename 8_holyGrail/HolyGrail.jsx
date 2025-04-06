c2c065542d13e7ca346e32a26cc93db27650ac2d
NAM Nguyen Hoang <hoangnamsdc@gmail.com>
2025-04-06 15:34:35 +0700

    finish holy grail

---
+++ 8_holyGrail/HolyGrail.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+export default function HolyGrail() {
+    return (
+      <div className="holy-grail ">
+        <header>Header</header>
+        <div className="content-section">
+          <nav>Navigation</nav>
+          <main>Main</main>
+          <aside>Sidebar</aside>
+        </div>
+        <footer>Footer</footer>
+      </div>
+    );
+  }
+  
--- index.jsx
+++ index.jsx
@@ -1,36 +1,38 @@
-import debounce from "./1_debounce";
-console.log("start time")
-const debounceLog = debounce(console.log, 1000)
-debounceLog("hello world 1st")
-debounceLog("hello world 2nd")
-debounceLog("hello world 3rd")
-debounceLog("hello world 4th")
+// import debounce from "./1_debounce";
+// console.log("start time")
+// const debounceLog = debounce(console.log, 1000)
+// debounceLog("hello world 1st")
+// debounceLog("hello world 2nd")
+// debounceLog("hello world 3rd")
+// debounceLog("hello world 4th")
 
-import myReduce from "./2_myReduce";
-console.log([1, 2, 3].myReduce((acc, cur) => acc + cur, 0));
+// import myReduce from "./2_myReduce";
+// console.log([1, 2, 3].myReduce((acc, cur) => acc + cur, 0));
 
-import classNames from "./3_className"
-console.log(classNames(null, false, 'bar', undefined, { baz: null }, ''))
+// import classNames from "./3_className"
+// console.log(classNames(null, false, 'bar', undefined, { baz: null }, ''))
 
-import flatten from "./4_myFlat";
-console.log(flatten([1,2,[3,[4],5,[6,[7,[8]],9]]]))
+// import flatten from "./4_myFlat";
+// console.log(flatten([1,2,[3,[4],5,[6,[7,[8]],9]]]))
 
-import throttle from "./5_throttle"
-const throttleLog = throttle((a,b) => console.log(a+b), 10)
-throttleLog(1,3)
+// import throttle from "./5_throttle"
+// const throttleLog = throttle((a,b) => console.log(a+b), 10)
+// throttleLog(1,3)
 
 
 import {createRoot} from 'react-dom/client'
 import React from 'react'
 import Todo from "./6_todoList/Todo";
 import ContactForm from "./7_contactForm/ContactForm";
+import HolyGrail from "./8_holyGrail/HolyGrail";
 
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(
     <>
-        <Todo />
-        <ContactForm />
+        {/* <Todo />
+        <ContactForm /> */}
+        <HolyGrail />
     </>
 )
