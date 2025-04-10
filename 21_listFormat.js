b08a31b735cca402a8c7d5f465a547ea612ab788
NAM Nguyen Hoang <hoangnamsdc@gmail.com>
2025-04-10 17:16:25 +0700

    Update 21_listFormat.js

--- 21_listFormat.js
+++ 21_listFormat.js
@@ -4,42 +4,43 @@
  * @return {string}
  */
 export default function listFormat(items, options) {
-    let resultString = "";
-  
-    if (!items.length) {
-      return resultString;
-    }
-  
-    if (items.length === 1) {
-      return items.toString();
-    }
-  
-    if (options) {
-      if (options.sorted) {
-        items = items.sorted();
-      }
-      if (options.unique) {
-        items = [...new Set(items)];
-      }
-      if (typeof options.length === "number") {
-        const remaining = items.length - options.length;
-        items = items.slice(0, options.length);
-        resultString = `${items.join(", ")} and ${remaining} other${remaining > 1 ? "s" : ""}`;
-      }
-      return resultString;
-    }
-  
-    const last = items.at(-1);
-    items = items.slice(0, items.length - 1);
-    resultString = `${items.join(", ")} and ${last}`;
-  
+  let resultString = "";
+
+  // filter empty items
+  items = items.filter((x) => x !== "");
+
+  // early return when array is empty
+  if (!items.length) {
     return resultString;
   }
-  
-  console.log(
-    listFormat(["Bob", "Ben", "Tim", "Jane", "John", "Bob"], {
-      length: 3,
-      unique: true,
-    }),
-  ); // 'Bob, Ben, Tim and 2 others'
-  
+
+  // early return when array has only 1 item
+  if (items.length === 1) {
+    return items.toString();
+  }
+
+  // apply options
+  if (options) {
+    if (options.sorted) {
+      items.sort();
+    }
+    if (options.unique) {
+      items = [...new Set(items)];
+    }
+
+    //only length option has to return early because of different logic for resultString
+    if (typeof options.length === "number") {
+      const remaining = items.length - options.length;
+      if (remaining > 0 && options.length > 0) {
+        items = items.slice(0, options.length);
+        resultString = `${items.join(", ")} and ${remaining} other${remaining > 1 ? "s" : ""}`;
+        return resultString;
+      }
+    }
+  }
+  const last = items.at(-1);
+  items = items.slice(0, items.length - 1);
+  resultString = `${items.join(", ")} and ${last}`;
+
+  return resultString;
+}
