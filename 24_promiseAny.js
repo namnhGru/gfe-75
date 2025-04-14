ca6f3bb9cdb1a83b96b842656e67636a9cb2dbef
NAM Nguyen Hoang <hoangnamsdc@gmail.com>
2025-04-14 13:50:17 +0700

    finish promiseAny

--- 24_promiseAny.js
+++ 24_promiseAny.js
@@ -3,20 +3,31 @@
  * @return {Promise}
  */
 export default function promiseAny(iterable) {
-    if (!iterable.length) {
-      return new AggregateError([]);
-    }
-  
-    const errorArray = [];
-  
-    return new Promise((resolve, rejected) => {
-      iterable.forEach((item, idx) => {
-        if (item instanceof Promise) {
-          item.then(resolve).catch((err) => rejected(err));
-        } else {
-          resolve(item);
-        }
-      });
-    });
+  // check if iterable isn't empty
+  if (!iterable.length) {
+    return new AggregateError([]);
   }
-  
+
+  // when all promise failed, return the failed promise
+  const errorArray = [];
+
+  // check how many time promise failed
+  let pending = 0;
+
+  // return a promise that can be chained
+  return new Promise((resolve, rejected) => {
+    iterable.forEach((item, idx) => {
+      // each item can be a pending promise, a resolved promise and a pure value.
+      // wrap item inside promise for more reliable
+      Promise.resolve(item)
+        .then(resolve)
+        .catch((err) => {
+          errorArray[idx] = err;
+          pending++;
+          if (pending === iterable.length) {
+            rejected(new AggregateError(errorArray, "all rejected"));
+          }
+        });
+    });
+  });
+}
